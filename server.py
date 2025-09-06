08bd79a3dfb41fc5a11df05d76dc705ece5a7634
Maryna Longnickel <maryna.longnickel@gmail.com>
2025-09-06 11:19:48 -0500

    wip

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 from datetime import date
-from flask import Flask
+from flask import Flask, jsonify, request
 from sqlalchemy import (
     create_engine,
     Column,
@@ -29,6 +29,8 @@ class User(Base):
     password = Column(String(300), nullable=False)
     expenses = relationship('Expense', back_populates='user') # user.expenses, list all
 
+# {"username": "Marina", "password": "123abc", "expenses": NaN}
+
 class Expense(Base):
     __tablename__ = "expenses"
     id = Column(Integer, primary_key=True)
@@ -38,10 +40,59 @@ class Expense(Base):
     date = Column(Date, nullable=False, default=date.today)
     category = Column(Enum('Food', 'Education', 'Entertainment'), nullable=False)
     user_id = Column(Integer, ForeignKey('users.id')) # Foreign key
-    user = relationship('User', back_populates='expense') # expense.user.username
+    user = relationship('User', back_populates='expenses') # expense.user.username
 
 # Create tables
 Base.metadata.create_all(engine)
+
+# Health check route
+@app.get('/api/health')
+
+def health_check():
+    return jsonify({"status": "ok"}), 200
+
+# User routes
+@app.post('/api/register')
+
+def register():
+    data = request.get_json()
+    username = data.get('username').lower().strip()  # normalize
+    password = data.get('password')
+
+    # Validation
+    existing_user = session.query(User).filter_by(username=username).first()
+
+    if existing_user:
+        return jsonify({"error": "Username already exists"}), 400
+
+    print(data)
+    print(username)
+    print(password)
+
+    new_user = User(username=username, password=password) # new User instance
+    session.add(new_user)
+    session.commit() # commit to DB
+
+    return jsonify({"status": "Created"}), 201
+
+# Login
+@app.post('/api/login')
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({"message": "Username or password missing"}), 400
+    
+    user = session.query(User).filter_by(username=username).first()
+
+    if user and user.password == password:
+        return jsonify({"message": "Login successful."}), 200
+    else: 
+        return jsonify({"message": "Wrong password."}), 401
+
+    return 'login'
 
 # Ensures the server runs ony when this script is executed directly
 if __name__ == "__main__":
